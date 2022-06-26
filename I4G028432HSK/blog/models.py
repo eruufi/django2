cb7c6093fc1e77a3612ca447593bc8c71a1ab6ea
eruufi <udk.eru14@gmail.com>
2022-06-26 12:59:37 +0100

    fix

--- I4G028432HSK/blog/models.py
+++ I4G028432HSK/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+
+class Post(models.Model):
+    Title = models.CharField(max_length=200)
+    Text = models.TextField()
+    Author = models.ForeignKey(User, on_delete=models.CASCADE)
+    Created_Date = models.DateTimeField()
+    Published_Date = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return self.Title
